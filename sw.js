2fa87b9e19bddf4387428b4ee0846cc405369279
plannermayur <plannermayur@gmail.com>
2026-08-30 15:50:50 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'aura-diamond-v3';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
